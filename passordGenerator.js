6f372ff7038a09301bb7bdb2435e1ef5ba44dd5d
SondreHaugom <sondrehaugom@gmail.com>
2025-10-16 14:57:53 +0200

    Translate alert messages to Norwegian for better localization

--- passordGenerator.js
+++ passordGenerator.js
@@ -11,7 +11,7 @@ btn.addEventListener("click", function() {
 
 function generatePassword(length) {
     if (length < 8 || length > 20) {
-        alert("Password length must be between 8 and 20 characters.");
+        alert("Passordlengde må være mellom 8 og 20 tegn.");
         return;
     }
     let charset = "abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
@@ -29,10 +29,10 @@ copyBtn.addEventListener("click", function () {
     let password = result.textContent;
     if (password) {
         navigator.clipboard.writeText(password).then(function() {
-            alert("Password copied to clipboard!");
+            alert("Passord kopiert!");
         }); 
     } else {
-        alert("No password to copy!");
+        alert("Ingen passord å kopiere!");
     }
 }); 
 
